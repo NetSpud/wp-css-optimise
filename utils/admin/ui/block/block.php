40c52c98e57cb7b0c79fdf1709e9036fb40834cd
Netspud <edward@themorrisagency.co.uk>
2024-11-29 23:26:50 +0000

    current dev

--- utils/admin/ui/block/block.php
+++ utils/admin/ui/block/block.php
@@ -1,27 +1,25 @@
 <?php
 
 
-function load_scripts()
+
+
+
+
+function sidebar_plugin_script_enqueue()
 {
     $asset_file = include(plugin_dir_path(__FILE__) . '../../../../build/index.asset.php');
-
-    // wp_enqueue_script(
-    //     'example-editor-scripts',
-    //     plugins_url('../../../build/index.js', __FILE__),
-    //     $asset_file['dependencies'],
-    //     $asset_file['version']
-    // );
-
-    wp_enqueue_script(
+    wp_register_script(
         'example-editor-scripts',
         plugins_url('../../../../build/index.js', __FILE__),
         array('wp-plugins', 'wp-editor', "wp-edit-post", "wp-components", 'react'),
         $asset_file['version']
     );
-    // wp_register_style(
-    //     'plugin-sidebar-css',
-    //     plugins_url('../../../style.css', __FILE__)
-    // );
-    // wp_enqueue_style('plugin-sidebar-css');
+    wp_enqueue_script('example-editor-scripts');
 }
-add_action('enqueue_block_editor_assets', 'load_scripts');
+add_action('enqueue_block_editor_assets', 'sidebar_plugin_script_enqueue');
+
+// function load_scripts()
+// {
+//     $asset_file = include(plugin_dir_path(__FILE__) . '../../../../build/index.asset.php');
+// }
+// add_action('enqueue_block_editor_assets', 'load_scripts');
--- utils/admin/ui/block/index.js
+++ utils/admin/ui/block/index.js
@@ -1,25 +1,50 @@
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginDocumentSettingPanel } from '@wordpress/editor';
-import { Button } from '@wordpress/components';
 
+import { useState } from 'react';
+import { MenuGroup, MenuItemsChoice } from '@wordpress/components';
 
-const PluginDocumentSettingPanelDemo = () => (
+const MyMenuItemsChoice = () => {
+    const [mode, setMode] = useState('default');
+    const choices = [
+        {
+            value: 'performance',
+            label: 'Performance Mode',
+        },
+        {
+            value: 'default',
+            label: 'Default Mode',
+        },
+    ];
+
+    return (
+        <MenuGroup label={"selected mode: " + mode}>
+            <MenuItemsChoice
+                choices={choices}
+                value={mode}
+                onSelect={(newMode) => setMode(newMode)}
+            />
+        </MenuGroup >
+    );
+};
+
+const SettingsPanel = () => (
     <PluginDocumentSettingPanel
         name="css-optimiser-sidebar"
         title="CSS Optimisation"
         className="css-optimiser-sidebar"
     >
 
-        <p>Custom Document Setting Panel</p>
-
+        <p>Select optimisation profile</p>
+        <MyMenuItemsChoice />
         {/* USEFUL COMMAND TO GET CURRENT METADATA:   wp.data.select( 'core/editor' ).getCurrentPost().meta;     */}
-        <Button variant="primary">Click Me!</Button>
+        {/* <Button variant="primary">Click Me!</Button> */}
 
 
     </PluginDocumentSettingPanel>
 );
 
-registerPlugin('plugin-document-setting-panel-demo', {
-    render: PluginDocumentSettingPanelDemo,
+registerPlugin('css-optimise-setting-panel', {
+    render: SettingsPanel,
     icon: 'palmtree',
 });
